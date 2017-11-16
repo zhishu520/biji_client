89009fdb6230104073a50109b059737d76b9941f
zhishu520 <569828992@qq.com>
2017-11-17 00:45:30 +0800

    添加选取卡牌的功能

--- Assets/Script/UI/Card.cs
+++ Assets/Script/UI/Card.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 
 class CardSpriteManager
 {
-	private Dictionary<string, Sprite> spriteDict = new Dictionary<string, Sprite>();
+	private Dictionary<string, Sprite> spriteDict = new Dictionary<string, Sprite> ();
 	static private CardSpriteManager instance;
-	static public CardSpriteManager Instance{
+
+	static public CardSpriteManager Instance {
 		get {
 			if (instance == null) {
 				instance = new CardSpriteManager ();
@@ -17,42 +18,76 @@ class CardSpriteManager
 		}
 	}
 
-	private void init() {
+	private void init ()
+	{
 		Sprite[] allCardSprite = Resources.LoadAll<Sprite> ("Texture/game_pai_ch1");
 		foreach (Sprite sprite in allCardSprite) {
-			spriteDict.Add(sprite.name, sprite);
+			spriteDict.Add (sprite.name, sprite);
 		} 
 	}
 
-	public Sprite getCardSprite(string name){
+	public Sprite getCardSprite (string name)
+	{
 		Debug.Log (name);
-		return spriteDict[name];
+		return spriteDict [name];
 	}
 }
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
 
 	public SpriteRenderer bgSprite;
 	public SpriteRenderer numSprite;
 	public SpriteRenderer suitSprite;
 
-
-	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		orgPosition = this.transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
-	public void SetCard(int value)
-	{
-		int suit = 	(value & 0xf00) >> 8;
-		int num = 	value & 0x0ff;
+	bool isUpState = false;
+	Vector3 orgPosition;
+	bool isTouch = false;
 
-		suitSprite.sprite = CardSpriteManager.Instance.getCardSprite("suit" + suit);
+	public void setTouchState(bool isTouch)
+	{
+		this.isTouch = isTouch;
+		bool isUp = isTouch ? !isUpState : isUpState;
+		this.transform.position = isUp ? orgPosition + new Vector3(0.0f, 0.2f, 0.0f) : orgPosition;
+	}
+
+	public void onTouchEnd()
+	{
+		if (isTouch)
+			isUpState = !isUpState;
+	}
+
+	public void changeUpDownState ()
+	{
+		
+	}
+
+	public bool IsContainTouchPosition (Vector2 position)
+	{
+		Bounds bounds = bgSprite.bounds;
+		bounds.center = this.transform.position;
+
+		Vector3 convert = new Vector3 (position.x, position.y, bgSprite.transform.position.z);
+		return bounds.Contains (convert);
+	}
+
+	public void SetCard (int value)
+	{
+		int suit = (value & 0xf00) >> 8;
+		int num = value & 0x0ff;
+
+		suitSprite.sprite = CardSpriteManager.Instance.getCardSprite ("suit" + suit);
 		string numSpriteName = suit % 2 == 1 ? "cardb" + num : "cardr" + num;
-		numSprite.sprite = CardSpriteManager.Instance.getCardSprite(numSpriteName);
+		numSprite.sprite = CardSpriteManager.Instance.getCardSprite (numSpriteName);
 	}
 }
--- Assets/Script/UI/CardGroup.cs
+++ Assets/Script/UI/CardGroup.cs
@@ -5,13 +5,91 @@ using UnityEngine;
 public class CardGroup : MonoBehaviour {
 
 	public static int GroupCardsNum = 9;
-	// Use this for initialization
+
+	private Card[] cardViews = new Card[GroupCardsNum];
+
+	Vector3 touchStartPosition;
+
+
 	void Start () {
+		int[] cards = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		InitByCardIds (cards);
 	}
-	
-	// Update is called once per frame
+
+
+	void SelectCard() {
+
+	}
+
+
+	bool isLastMouseDown = false;
+	int touchStartIndex = 0;
+
+	public Camera mainCamera;
+	int getCardIndexByTouchPosition(Vector3 pos) {
+		var wpos = mainCamera.ScreenToWorldPoint (pos);
+		for (int i = GroupCardsNum-1; i >=0 ; i-- ){
+			if (cardViews[i].IsContainTouchPosition(wpos)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void CheckTouchCard() {
+		var touch = Input.GetTouch (0);
+		if (touch.phase == TouchPhase.Began) {
+			touchStartPosition = touch.position;
+			touchStartIndex = getCardIndexByTouchPosition (touchStartPosition);
+		}
+
+		int index = getCardIndexByTouchPosition(touch.position);
+		if (index == -1)
+			return;
+
+		int a = Mathf.Min (touchStartIndex, index);
+		int b = Mathf.Max (touchStartIndex, index);
+
+		for (int i = 0; i < GroupCardsNum; i++)
+			cardViews [i].setTouchState (i>=a && i<=b);
+
+		isLastMouseDown = true;
+	}
+
+	void CheckMouseCard()
+	{
+		if (!isLastMouseDown) {
+			touchStartPosition = Input.mousePosition;
+			touchStartIndex = getCardIndexByTouchPosition (touchStartPosition);
+		}
+
+		int index = getCardIndexByTouchPosition(Input.mousePosition);
+		if (index == -1)
+			return;
+
+		int a = Mathf.Min (touchStartIndex, index);
+		int b = Mathf.Max (touchStartIndex, index);
+
+		for (int i = 0; i < GroupCardsNum; i++)
+			cardViews [i].setTouchState (i>=a && i<=b);
+
+		isLastMouseDown = true;
+	}
+
+
 	void Update () {
-		
+		if (Input.touchCount > 0) {
+			CheckTouchCard ();
+		}
+
+		if(Input.GetMouseButton(0))
+			CheckMouseCard ();
+
+		if (Input.GetMouseButtonUp (0)) {
+			isLastMouseDown = false;
+			for (int i = 0; i < GroupCardsNum; i++)
+				cardViews [i].onTouchEnd ();
+		}
 	}
 
 	public void InitByCardIds(int[] cards)
@@ -22,14 +100,16 @@ public class CardGroup : MonoBehaviour {
 		var cardPrefab = (GameObject)Resources.Load ("Prefab/card");
 
 		for (int i = 0; i < GroupCardsNum; i++) {
-			var card = (GameObject)Instantiate (cardPrefab);
-			card.transform.parent = this.transform;
-			card.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+			var obj = (GameObject)Instantiate (cardPrefab);
+			obj.transform.parent = this.transform;
+			obj.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
 
-			float x = 5.675f + (4-i)* 0.5f  ; 
-			card.transform.position = new Vector3 (x,1.35f,i* 0.5f);
+			float x = 5.675f + (i-4)* 0.5f  ; 
+			obj.transform.position = new Vector3 (x,1.35f,-i* 0.5f);
 
-			card.GetComponent<Card> ().SetCard (cards[i]);
+			var card = obj.GetComponent<Card> ();
+			card.SetCard (cards[i]);
+			cardViews[i] = card;
 		}
 	}
 
